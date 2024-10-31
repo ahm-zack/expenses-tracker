32b8458f93c9366b2207150d69536efc7cf89f01
ahm-zack <azaaaadh@gmail.com>
2024-10-31 14:57:43 +0300

    ui designs added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import TransactionPage from "./pages/TransactionPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import Header from "./components/ui/Header";
+
 function App() {
+  const authUser = true;
   return (
     <>
-      <h1 className="text-red-500 text-3xl">hello world</h1>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
